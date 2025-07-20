3f82ce31479784ace451a59115dda071b0367621
BJL156 <bjl15634@gmail.com>
2025-07-19 21:15:13 -0400

    Added ability to enable paging.

--- src/kernel.c
+++ src/kernel.c
@@ -6,15 +6,21 @@
 #include "cpu/pic.h"
 #include "cpu/interrupts.h"
 
+#include "memory/paging.h"
+
 #include "shell.h"
 
 void kernel_main() {
   gdt_install();
   idt_install();
   pic_remap();
+  paging_init();
   vga_init();
   keyboard_init();
   enable_interrupts();
+
+  volatile uint32_t *test = (uint32_t*)0x200000;
+  *test = 0xBEEF1234;
 
   shell();
 }
